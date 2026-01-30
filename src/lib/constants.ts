14d67f8550b0a3e386da42ae47c620417c8d09d7
ShiruiSan <harlampevrenat@gmail.com>
2026-01-31 00:58:30 +0900

    fix

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const SITE_NAME = "ShiShop";
 export const SITE_TAGLINE = "Личные вещи и подборки из Китая";
 
-export const TELEGRAM_USERNAME = "shishop_demo";
+export const TELEGRAM_USERNAME = "ShiruiSan";
 
 export const ADMIN_SESSION_COOKIE = "shishop_session";
 
